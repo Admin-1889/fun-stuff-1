3b28adc8cbf1a05e0fe8c3c91d665e3b6c4ee0b5
whoami <72305591+Admin-1889@users.noreply.github.com>
2020-10-22 17:00:16 +0800

    Create shutdown.bat

---
+++ shutdown.bat
@@ -0,0 +1,9 @@
+# this is a .bat file
+# this causes computer to shutdown with a message, change your message here! to whatever you want
+
+code:
+
+@echo off
+Shutdown.exe -s -t 30
+cls
+msg * your message here!
