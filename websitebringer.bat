67d87969dca519cb846a64ed7bae151539f8fd92
whoami <72305591+Admin-1889@users.noreply.github.com>
2020-10-22 16:58:17 +0800

    Create websitebringer.bat

---
+++ websitebringer.bat
@@ -0,0 +1,13 @@
+# this is a .bat file
+# this launches website in user's default browser, change whateversite . com to actual websites
+
+
+code:
+
+:start
+@echo off
+cls
+start "" whateversite . com
+start "" whateversite . com
+timeout 8
+goto start
